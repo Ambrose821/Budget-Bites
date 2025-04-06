a39f1b38d894226731ae5de587cb323375fdd3fa
ambrose821 <mclaughlinambrose@gmail.com>
2025-04-06 11:56:24 -0700

    Expenses can now be added

--- Budget-Bites/app/(tabs)/index.tsx
+++ Budget-Bites/app/(tabs)/index.tsx
@@ -9,6 +9,7 @@ import {
   Touchable,
   TouchableOpacity,
   TextInput,
+  TouchableWithoutFeedback,
 } from 'react-native';
 import { Stack } from 'expo-router';
 
@@ -19,7 +20,10 @@ import ExpenseBlock from '@/components/ExpenseBlock';
 import ExpenseList from '@/data/expenses.json';
 import React, { useEffect, useState } from 'react';
 import { Picker } from '@react-native-picker/picker';
-import { BlurView } from '@react-native-community/blur';
+import { ExpenseType } from '@/types';
+
+import { BlurView } from 'expo-blur';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 export default function Index() {
   const [totalCost, setTotalCost] = useState<Number>(0);
 
@@ -29,7 +33,7 @@ export default function Index() {
       0
     );
     setTotalCost(cost);
-  }, [totalCost]);
+  }, [ExpenseList]);
 
   const [showForm, setShowForm] = useState<Boolean>(false);
 
@@ -37,7 +41,27 @@ export default function Index() {
   const [purchaseType, setPurchaseType] = useState('');
   const [purchaseAmount, setPurchaseAmount] = useState('');
 
-  const handleSubmit = () => {};
+  const handleSubmit = () => {
+    setShowForm(!showForm);
+    const newExpense = {
+      id: String(Date.now()),
+      name: purchaseName,
+      type: purchaseType,
+      amount: purchaseAmount,
+      percentage: '8',
+    };
+    console.log(newExpense);
+    ExpenseList.push(newExpense);
+    setPurchaseAmount('');
+    setPurchaseType('');
+    setPurchaseName('');
+
+    const cost = ExpenseList.reduce(
+      (accumulator, expense) => accumulator + Number(expense.amount),
+      0
+    );
+    setTotalCost(cost);
+  };
   const pieData = [
     {
       value: 47,
@@ -106,7 +130,7 @@ export default function Index() {
               />
             </View>
           </View>
-          <ExpenseBlock expenseList={ExpenseList}></ExpenseBlock>
+          <ExpenseBlock expenseList={ExpenseList.reverse()}></ExpenseBlock>
           <TouchableOpacity
             onPress={() => {
               setShowForm(!showForm);
@@ -130,77 +154,99 @@ export default function Index() {
       </View>
 
       {showForm && (
-        <View
-          style={{
-            position: 'absolute',
-            marginTop: 20,
-            backgroundColor: Colors.grey,
-            padding: 16,
-            borderRadius: 10,
-            alignSelf: 'center',
-            gap: 4,
-          }}>
-          <View style={{ flexDirection: 'row' }}>
-            <Text style={{ color: Colors.blue, fontSize: 20, fontWeight: 'bold' }}>Add</Text>
-            <Text style={{ color: 'white', fontSize: 20, fontWeight: 'bold' }}> Purchase</Text>
+        <>
+          <TouchableWithoutFeedback onPress={() => setShowForm(!showForm)}>
+            <BlurView style={StyleSheet.absoluteFill} tint="dark" intensity={80} />
+          </TouchableWithoutFeedback>
+          <View
+            style={{
+              position: 'absolute',
+              marginTop: 20,
+              width: 300,
+              backgroundColor: Colors.grey,
+              padding: 16,
+              borderRadius: 10,
+              alignSelf: 'center',
+              gap: 4,
+              zIndex: 10,
+            }}>
+            <TouchableOpacity
+              onPress={() => setShowForm(!showForm)}
+              style={{
+                position: 'absolute',
+                right: 18,
+                top: 18,
+                width: 30,
+                height: 30,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: 20,
+                backgroundColor: Colors.tintColor,
+              }}>
+              <FontAwesome name="close" size={18} color="white" />
+            </TouchableOpacity>
+            <View style={{ flexDirection: 'row' }}>
+              <Text style={{ color: Colors.blue, fontSize: 20, fontWeight: 'bold' }}>Add</Text>
+              <Text style={{ color: 'white', fontSize: 20, fontWeight: 'bold' }}> Purchase</Text>
+            </View>
+
+            {/* Text Input */}
+            <Text style={{ color: 'white', marginBottom: 4 }}>Purchase Title</Text>
+            <TextInput
+              value={purchaseName}
+              onChangeText={setPurchaseName}
+              placeholder="Enter purchase title"
+              placeholderTextColor="#ccc"
+              style={{
+                backgroundColor: 'white',
+                padding: 8,
+                borderRadius: 5,
+                marginBottom: 12,
+              }}
+            />
+
+            <Text style={{ color: 'white', marginBottom: 4 }}>Amount</Text>
+            <TextInput
+              value={purchaseAmount}
+              onChangeText={setPurchaseAmount}
+              placeholder="Enter amount"
+              placeholderTextColor="#ccc"
+              keyboardType="numeric"
+              style={{
+                backgroundColor: 'white',
+                padding: 8,
+                borderRadius: 5,
+                marginBottom: 12,
+              }}
+            />
+
+            {/* Dropdown / Picker */}
+            <Text style={{ color: 'white', marginBottom: 4 }}>Category</Text>
+            <Picker
+              selectedValue={purchaseType}
+              onValueChange={(itemValue) => setPurchaseType(itemValue)}
+              style={{
+                backgroundColor: 'white',
+                marginBottom: 12,
+              }}>
+              <Picker.Item label="Select category" value="" />
+              <Picker.Item label="Grocery" value="food" />
+              <Picker.Item label="Eat Out" value="eat out" />
+            </Picker>
+
+            {/* Submit Button */}
+            <TouchableOpacity
+              onPress={() => handleSubmit()}
+              style={{
+                backgroundColor: Colors.tintColor,
+                padding: 10,
+                borderRadius: 5,
+                alignItems: 'center',
+              }}>
+              <Text style={{ color: 'white', fontWeight: 'bold' }}>Add Expense</Text>
+            </TouchableOpacity>
           </View>
-
-          {/* Text Input */}
-          <Text style={{ color: 'white', marginBottom: 4 }}>Purchase Title</Text>
-          <TextInput
-            value={purchaseName}
-            onChangeText={setPurchaseName}
-            placeholder="Enter purchase title"
-            placeholderTextColor="#ccc"
-            style={{
-              backgroundColor: 'white',
-              padding: 8,
-              borderRadius: 5,
-              marginBottom: 12,
-            }}
-          />
-
-          <Text style={{ color: 'white', marginBottom: 4 }}>Amount</Text>
-          <TextInput
-            value={purchaseAmount}
-            onChangeText={setPurchaseAmount}
-            placeholder="Enter amount"
-            placeholderTextColor="#ccc"
-            keyboardType="numeric"
-            style={{
-              backgroundColor: 'white',
-              padding: 8,
-              borderRadius: 5,
-              marginBottom: 12,
-            }}
-          />
-
-          {/* Dropdown / Picker */}
-          <Text style={{ color: 'white', marginBottom: 4 }}>Category</Text>
-          <Picker
-            selectedValue={purchaseType}
-            onValueChange={(itemValue) => setPurchaseType(itemValue)}
-            style={{
-              backgroundColor: 'white',
-              marginBottom: 12,
-            }}>
-            <Picker.Item label="Select category" value="" />
-            <Picker.Item label="Grocery" value="food" />
-            <Picker.Item label="Eat Out" value="eat out" />
-          </Picker>
-
-          {/* Submit Button */}
-          <TouchableOpacity
-            onPress={() => handleSubmit()}
-            style={{
-              backgroundColor: Colors.tintColor,
-              padding: 10,
-              borderRadius: 5,
-              alignItems: 'center',
-            }}>
-            <Text style={{ color: 'white', fontWeight: 'bold' }}>Add Expense</Text>
-          </TouchableOpacity>
-        </View>
+        </>
       )}
     </>
   );
